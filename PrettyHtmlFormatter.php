edb1c71d891c6decb9cbddaff4021f566cb0ad77
Julien <julien.bouisset@digitregroup.com>
2018-04-27 11:59:24 +0200

    Created Twig templates

--- PrettyHtmlFormatter.php
+++ PrettyHtmlFormatter.php
@@ -4,32 +4,46 @@ namespace Grav\Plugin;
 
 class PrettyHtmlFormatter extends \Monolog\Formatter\HtmlFormatter
 {
+    /** @var \Twig_Environment $twig */
+    private $twig;
+
     /**
      * Formats a log record.
      * @param  array $record A record to format
      * @return mixed The formatted record
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
      */
     public function format(array $record)
     {
-        $template = <<<HTML
-<div style="font-family: 'DejaVu Sans Mono', Menlo, Monaco, Consolas, Courier, monospace;font-size: .8em">
-{{LEVEL}}
-<h2>{{TITLE}}</h2>
-{{CONTEXT}}
-{{EXTRA}}
-<pre style="font-size: 1.2em">🕒 {{DATETIME}}</pre>
-{{STACKTRACE}}
-</div>
-HTML;
+        return $this->renderTwigString('email.twig', [
+            'level'      => $this->addTitle($record['level_name'], $record['level']),
+            'title'      => substr($record['message'], 0, strpos($record['message'], ' - Trace:')),
+            'context'    => $this->formatExtra($record, 'context'),
+            'extra'      => $this->formatExtra($record, 'extra'),
+            'datetime'   => $record['datetime']->format($this->dateFormat),
+            'stacktrace' => $this->formatStacktrace($record['message']),
+        ]);
+    }
 
-        return str_replace(['{{LEVEL}}', '{{TITLE}}', '{{CONTEXT}}', '{{EXTRA}}', '{{DATETIME}}', '{{STACKTRACE}}'], [
-            $this->addTitle($record['level_name'], $record['level']), // LEVEL
-            substr($record['message'], 0, strpos($record['message'], ' - Trace:')), // TITLE
-            $this->formatExtra($record, 'context'), // CONTEXT
-            $this->formatExtra($record, 'extra'), // EXTRA
-            $record['datetime']->format($this->dateFormat), // DATETIME
-            $this->formatStacktrace($record['message']), // STACKTRACE
-        ], $template);
+    /**
+     * @param $templateString
+     * @param array $data
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    private function renderTwigString($templateString, $data = [])
+    {
+        if (null === $this->twig) {
+            $this->twig = new \Twig_Environment(
+                new \Twig_Loader_Filesystem(__DIR__ . '/twig'),
+                ['autoescape' => false]
+            );
+        }
+        return $this->twig->render($templateString, $data);
     }
 
     /**
@@ -37,6 +51,9 @@ HTML;
      * @param array $record A record to format
      * @param string $recordKey The array key of the block in the record
      * @return string The formatted block
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
      */
     private function formatExtra($record, $recordKey)
     {
@@ -52,34 +69,32 @@ HTML;
             array_values($record[$recordKey])
         );
 
-        return sprintf('<table cellspacing="1" width="100%%">%s</table>', implode('', $rows));
+        return $this->renderTwigString('extra.twig', ['rows' => $rows]);
     }
 
     /**
      * Formats the stacktrace from a message into a syntax-colored table
      * @param string $message The message containing the stacktrace
      * @return string The formatted stacktrace
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
      */
     private function formatStacktrace($message)
     {
         // Parse stacktrace
         if (!preg_match_all('/#[\d]+ (.+): ([^\n]+)/', $message, $matches, PREG_SET_ORDER)) {
             // Simple message without stacktrace
-            return sprintf('<div style="font-weight: bold;font-size: 1.2em;">%s</div>', $message);
+            return $this->renderTwigString('simple-message.twig', ['message' => $message]);
         }
 
-        $template = <<<HTML
- <div style="padding: .5em 0;">
-    <span style="font-size: 1.3em;">%s</span>
-    <div style="color: #999;;">%s</div>
-</div>
-HTML;
-
-        return implode('', array_map(function ($frame) use ($template) {
-
+        return implode('', array_map(function ($frame) {
             // Process each stacktrace line
             list(, $path, $method) = $frame;
-            return sprintf($template, $this->colorMethodSyntax($method), $path);
+            return $this->renderTwigString('stacktrace-line.twig', [
+                'method' => $this->colorMethodSyntax($method),
+                'path'   => $path
+            ]);
 
         }, $matches));
 
